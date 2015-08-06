3bdc3966efded5c14792918c84790ce81167d32e
Joe P <joe@peled.nyc>
2015-08-06 11:40:20 -0400

    graphite stuff

---
+++ graphite/graphitify.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+    echo "usage: `basename $0` <metricname> <metric> <unix timestamp>"
+    exit 0
+fi
+
+if [ ! -x /usr/bin/nc -a ! -x /bin/nc ]; then
+        echo "`basename $0` requires netcat"
+        exit 1
+fi
+
+TS=$3
+if [[ $TS = "" || $TS -lt 0 || $TS -gt $(date +%s) ]]; then
+    TS=$(date +%s)
+fi
+
+echo "$1 $2 $TS" | nc carbon.df.vimeows.com 2003
